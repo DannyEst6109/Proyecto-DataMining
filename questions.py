8eedfcc57cef2b64faecd6ada8f3a299bbf8d0d9
Hansel López <halm29799@gmail.com>
2024-03-17 20:10:53 -0600

    Add questions

---
+++ questions.py
@@ -0,0 +1,34 @@
+# questions.py
+
+# Tendencias Temporales:
+# 1. ¿Cómo ha evolucionado la tasa de mortalidad general a lo largo de los años?
+
+# Causas de Defunción:
+# 2. ¿Cuáles son las principales causas de defunción registradas cada año?
+# 3. ¿Ha habido cambios significativos en las principales causas de defunción a lo largo del tiempo?
+
+# Demografía:
+# 4. ¿Existe alguna diferencia notable en las causas de defunción entre diferentes grupos de edad?
+# 5. ¿Cómo varían las tasas de mortalidad por género a lo largo de los años?
+
+# Geografía:
+# 6. ¿Hay diferencias en las tasas de mortalidad o causas de defunción entre diferentes regiones o ciudades?
+
+# Análisis Temporal Específico:
+# 7. ¿Se observa algún patrón estacional o mensual en las defunciones a lo largo de los años?
+
+# Comparaciones y Correlaciones:
+# 8. ¿Existe alguna correlación entre la edad al momento de la defunción y causas específicas de muerte?
+# 9. ¿Cómo afectan factores como el estado civil o el nivel educativo a las tasas de mortalidad?
+
+# Anomalías y Picos:
+# 10. ¿Se identifican años con picos anómalos en las defunciones? ¿A qué se pueden atribuir?
+
+# Tendencias Recientes:
+# 11. ¿Cómo ha impactado la pandemia de COVID-19 en las tasas de mortalidad y causas de defunción en los últimos años disponibles?
+
+# Longevidad:
+# 12. ¿Ha habido un cambio en la esperanza de vida al momento de la defunción a lo largo de los años?
+
+# Prevención y Salud Pública:
+# 13. Basado en las causas de defunción más comunes, ¿qué áreas podrían ser prioritarias para intervenciones de salud pública?
